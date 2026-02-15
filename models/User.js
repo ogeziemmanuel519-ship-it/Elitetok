340e55778cb0f9289df0cf731d620529c433fcdf
ogeziemmanuel519-ship-it <ogeziemmanuel519@gmail.com>
2026-02-15 02:26:50 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  points: { type: Number, default: 10 }, // new users get 10 points
+  referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   payments: [
     {
       amount: Number,
